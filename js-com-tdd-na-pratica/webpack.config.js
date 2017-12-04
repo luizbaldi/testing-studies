ec151b0d852e174085be61b6818602b8518cd576
Luiz Baldi <baldilp@gmail.com>
2017-12-04 20:28:14 -0200

    Initial commit for "js com tdd na pratica" course Add basic envirionment setup with .editorconfig and webpack config files

---
+++ js-com-tdd-na-pratica/webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    filename: './app.js'
+  },
+  output: {
+    filename: './build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: '/\.js$/',
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false }
+    })
+  ]
+}
