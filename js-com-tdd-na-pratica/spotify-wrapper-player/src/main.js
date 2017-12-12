870da5aeb896b620ad218968c51a41812f0f32c6
Luiz Baldi <baldilp@gmail.com>
2017-12-12 21:11:57 -0200

    Create folder with environment prepared with webpack Everything ready to start developing the spotify player

---
+++ js-com-tdd-na-pratica/spotify-wrapper-player/src/main.js
@@ -0,0 +1,1 @@
+console.log('hurray!!!!');
---
+++ js-com-tdd-na-pratica/spotify-wrapper-player/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './main.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader'
+        }]
+      }
+    ]
+  }
+};
