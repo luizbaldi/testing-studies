3e4924f4f0420c80d42133219dd8777d680209b5
Luiz Baldi <baldilp@gmail.com>
2017-12-12 23:28:42 -0200

    Create index.html structure and main components using only es6 Also crate tests for all component render methods using jsdom

--- js-com-tdd-na-pratica/spotify-wrapper-player/src/main.js
+++ js-com-tdd-na-pratica/spotify-wrapper-player/src/main.js
@@ -1,1 +1,19 @@
-console.log('hurray!!!!');
+import spotify from './spotify';
+import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
+
+/* Creating elements and fetching data using api */
+const albums = spotify.search.albums('Bon+Iver');
+const albumListEl = document.getElementById('album-list');
+
+const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
+const albumInfoEl = document.getElementById('album-info');
+
+const tracks = spotify.album.getTracks('6peEdPVO73WtgGah5sEhX4');
+const albumTracksEl = document.getElementById('album-tracks');
+
+/* Rendering data */
+albums.then(data => renderAlbums(data.albums.items, albumListEl));
+album.then(data => renderAlbumInfo(data, albumInfoEl));
+tracks.then(data => renderAlbumTracks(data.items, albumTracksEl));
---
+++ js-com-tdd-na-pratica/spotify-wrapper-player/src/spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQBcXdYoRCY8PeXSI22EVOcbZvfX0XMHqxJBGYt8X3e60geQM84-WV39vFZMlwrPbwVo6rpOU5IRmBid8F_BpSX_dQwumQMPkyB-kL7f1k2MyETzGDwZd1FUacLoUrh_EtRv3ehrxFEgaST5'
+});
+
+export default spotify;
