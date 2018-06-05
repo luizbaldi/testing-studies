7cbde79cb615b3012368f6532792c8b59ebcd84b
Baldi <baldilp@gmail.com>
2018-06-04 22:34:58 -0300

    Add redux and connect components

--- advanced-react-and-redux/src/App.js
+++ advanced-react-and-redux/src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+
+/* Comments */
+import CommentBox from './components/CommentBox'
+import CommentList from './components/CommentList'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Hallo!</h2>
-      </div>
+      <Fragment>
+        <CommentBox />
+        <CommentList />
+      </Fragment>
     );
   }
 }
