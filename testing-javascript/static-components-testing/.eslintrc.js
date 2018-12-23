8acb170b63fbb7573be95b9d82b2c82bbc93d2cd
Baldi <baldilp@gmail.com>
2018-12-23 19:29:42 -0200

    Add jest structure for testing react components - Create simple input component - Create specs to test that simple input component with react-testing-libraryt

--- testing-javascript/static-components-testing/.eslintrc.js
+++ testing-javascript/static-components-testing/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
     extends: "airbnb",
+    plugins: [
+        "jest"
+    ],
     env: {
-        browser: true
+        browser: true,
+        "jest/globals": true
     },
     rules: {
         "comma-dangle": [2, "never"],
--- testing-javascript/static-components-testing/src/App.jsx
+++ testing-javascript/static-components-testing/src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from 'react'
+import NumberInput from './components/NumberInput'
 
 const App = () => (
   <div>
-    <span>Potatoe</span>
+    <NumberInput />
   </div>
 )
 
