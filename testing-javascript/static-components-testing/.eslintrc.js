cfdd107fef8fda8357843890445da14f6d0483c1
Baldi <baldilp@gmail.com>
2018-12-23 18:10:07 -0200

    Create initial structure for testing-javascript course project - Configure Parcel (which doesn't need a config) - Configure ESLint - Configure Babel

---
+++ testing-javascript/static-components-testing/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: "airbnb",
+    env: {
+        browser: true
+    },
+    rules: {
+        "comma-dangle": [2, "never"],
+        "semi": [2, "never"],
+        "react/jsx-filename-extension": "off"
+    }
+}
---
+++ testing-javascript/static-components-testing/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <span>Potatoe</span>
+  </div>
+)
+
+export default App
